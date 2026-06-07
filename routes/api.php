a1a1f5a449c5788a7dc01bb64f4fb29c1c92559e
akmalkhaleeed <2303010112@unper.ac.id>
2026-06-07 07:35:38 +0700

    feat: setup routing api untuk login dan logout

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// 🚪 ROUTE PUBLIK (Bisa diakses tanpa login/token oleh Diaz / Postman)
+Route::post('/login', [AuthController::class, 'login']);
+
+// 🔒 ROUTE PROTECTED (Hanya bisa diakses jika membawa Token Sanctum hasil login)
+Route::middleware('auth:sanctum')->group(function () {
+
+    // Jalur untuk Logout
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
